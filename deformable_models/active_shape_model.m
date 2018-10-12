a7a5c6d062f778dfba7932b9fe258df1f5f6dd5c
ahsanMah <mahmood.ahsan@outlook.com>
2018-10-11 22:00:41 -0400

    Divided PDMs into train/test and mean normalized

--- deformable_models/active_shape_model.m
+++ deformable_models/active_shape_model.m
@@ -1,6 +1,7 @@
 load('pdms.mat');
 load 'greyimages.mat' ;
 load 'correctpdms';
+rng(7);
 
 % figure();
 % greyimage =reshape(greyimages(:,2),256, 256); % *or binaryimage and binaryimages, respectively 
@@ -13,13 +14,21 @@ load 'correctpdms';
 
 normalized_pdms = pdms;
 
+pdms_3d = zeros(2,64,32);
+pdms_3d(1,:,:) = normalized_pdms(1:2:end,:);
+pdms_3d(2,:,:) = normalized_pdms(2:2:end,:);
 
+% Dividing into 3/4th training set and the rest as test cases
+training_set = pdms_3d(:,:,1:24);
+test_set = pdms_3d(:,:,25:32);
 
+% Take the average value across each x and y point in a test case
+mean_pdm = mean(training_set,3);
+norm_train_set = training_set - mean_pdm;
 
+%  Do PCA on the normalized training set
 
-
-
-function normalizePDMs(pdms):
+function normalizePDMs(pdms)
     normalized_pdms = zeros(128,32);
 
 x_means = mean(pdms(1:2:end,:));
@@ -41,3 +50,4 @@ matrix_3d(2,:,:) = normalized_pdms(2:2:end,:);
 
 rotation_matrix(1,1,:) = sum(matrix_3d(1,:,:).^2, 2)
 %%%%%%%%%%%%%%%%%%%%
+end
