39f8979a071b49c407df258cc6fcede24335ac1e
ahsanMah <mahmood.ahsan@outlook.com>
2018-12-12 12:32:31 -0500

    - Implemented two class stripping - Started work on a script to calculate class means

--- skull_stripping/constructGraph.m
+++ skull_stripping/constructGraph.m
@@ -10,38 +10,48 @@ function [G, super_src, sink] = constructGraph(dim, original_img, scheme)
     % Building graph
     G = imageGraph([dim,dim],4);
     G = digraph(G.Edges, G.Nodes)
-     
+    
+    prune = 0;
+    
+    if prune == 1
+    
     % Remove edges involving pixels with very low intensities
-    cutoff = 20
-    edges = G.Edges.EndNodes;   
-    rm_s = []; 
-    rm_t = [];
-    for edge = edges.' %Transpose the matrix to loop over rows
-        s = edge(1);
-        t = edge(2);
-        
-        if intensities(s) < cutoff && intensities(t) < cutoff
-            rm_s = [rm_s s]; rm_t = [rm_t t];
+        cutoff = 20
+        edges = G.Edges.EndNodes;   
+        rm_s = []; 
+        rm_t = [];
+        for edge = edges.' %Transpose the matrix to loop over rows
+            s = edge(1);
+            t = edge(2);
+
+            if intensities(s) < cutoff && intensities(t) < cutoff
+                rm_s = [rm_s s]; rm_t = [rm_t t];
+            end
         end
+
+        disp('After pruning...');
+        G = rmedge(G,rm_s, rm_t)
+
+        % Want bidirectional edges
+        edges   = G.Edges.EndNodes;
+        weights = G.Edges.Weight; % All default to 1
+
+        s_nodes = edges(:,1);
+        t_nodes = edges(:,2);
+        G = addedge(G,t_nodes, s_nodes, weights);
+
     end
     
-    disp('After pruning...');
-    G = rmedge(G,rm_s, rm_t)
-    % Remove nodes that are not connected to anything
-    G = rmnode()
-    
-    % Want bidirectional edges
-    edges   = G.Edges.EndNodes;
-    weights = G.Edges.Weight; % All default to 1
-    
-    s_nodes = edges(:,1);
-    t_nodes = edges(:,2);
-    G = addedge(G,t_nodes, s_nodes, weights);
-    
+%   % Remove nodes that are not connected to anything
+%     disp('Removing disconnected nodes...');
+%     indeg = indegree(G)==0;
+%     outdeg = outdegree(G)==0;
+%     lonely_nodes = G.Nodes.PixelIndex(indeg & outdeg);
+%     G = rmnode(G, lonely_nodes)
+%     
     
     [Gmag,Gdir] = imgradient(original_img);
 
-    
     gradients   = Gmag(:);
     edges = G.Edges.EndNodes;
     
@@ -54,25 +64,29 @@ function [G, super_src, sink] = constructGraph(dim, original_img, scheme)
 %     nodes = 1:num_pixels;
     
     % To ignore pixels outised the brain
-    nonzero_pixels = (intensities > 0);
     
     
-    background_pixels = intensities(nonzero_pixels);
-    foreground_pixels = background_pixels;
-    num_nonzero = size(background_pixels,1);
+    nodes = G.Nodes.PixelIndex;
+    node_intensities = intensities(nodes);
+      
+%     nonzero_pixels = (intensities > 0);
+%     background_pixels = intensities(nonzero_pixels);
+%     background_nodes = find(nonzero_pixels == 1);
+   
     
-    % Adding a super source with Pixel Index = Num Pixels + 1
-    super_src = zeros(num_nonzero,1) + num_pixels + 1;
+    foreground_pixels = node_intensities;
+    num_nodes = size(node_intensities,1)
     
-    % Adding a sink with Pixel Index = Num Pixels + 2
-    sink = zeros(num_nonzero,1) + num_pixels + 2;
-
-    
+   
     %Assuming background and foreground intensities
-    mean_background = 75
+    mean_background = 50
     
     % General heuristic as skull shows up lighter on MRI
     mean_target = 200
+    
+    WM_mean = 61;
+    WM_sd = 14;
+    mean_target = WM_mean; %white matter mean
 
     % Want high weights when connecting to background pixels 
     % If background -> High weight else Low
@@ -82,44 +96,73 @@ function [G, super_src, sink] = constructGraph(dim, original_img, scheme)
     % If target -> High weight else Low
     t_weight = ones(num_pixels,1);
     
+    MAX = 255;
     
     switch scheme
         
         case 1 % Linear Decay
             disp('Using Scheme 1')
-            s_weight = 255 ./ (1 + abs(intensities - mean_background));
+            s_weight = 255 ./ (1 + abs(node_intensities - mean_background));
   
-            t_weight = 255 ./ (1 + abs(intensities - mean_target));
+            t_weight = 255 ./ (1 + abs(node_intensities - mean_target));
         
         case 2
             s_weight = 255 ./ exp(abs(intensities - mean_background));
             t_weight = 255 ./ exp(abs(intensities - mean_target));
         
         case 3
-            original_img(original_img < 30) = 0 ; % Gets rid of the noise outside
-            s_weight = original_img;
-            t_weight = original_img;
+
+            % Target is brain
+            % Background is 0 and skull
             
-            s_weight(original_img > mean_target) = 0;
-            t_weight(original_img < mean_target) = 0;
+            sdw = 1*WM_sd;
+            wm_low = WM_mean-sdw; wm_high = WM_mean+sdw;
+            target_pixels = node_intensities > wm_low & node_intensities < wm_high;
+            
+            skull_mean = 180; skull_sd = 20;
+            sm_low = skull_mean - skull_sd;
+            bg_pixels = node_intensities > sm_low;
+            
+            s_weight = zeros(num_nodes,1);
+            
+%             s_weight(node_intensities < wm_low) = bg_low .* (bg_low - wm_low).^2;
+            max_weight = max(max(s_weight(:)), MAX);
+            s_weight(node_intensities == 0) = max_weight;
+            s_weight(bg_pixels) = max_weight;
+            
+            t_weight = node_intensities';
+%             t_weight = t_weight .* min(1, 1./(t_weight - mean_target).^2);
+            t_weight = t_weight .* min(1, 1./abs(t_weight - mean_target));
+            t_weight(node_intensities < 1) = 0; %Not conected to background
+            t_weight(target_pixels) = MAX;
         
         case 4
+            %Assuming background and foreground intensities
+            mean_background = 50;
+            mean_target = 200;
             
-            s_weight = background_pixels .* min(1, 1./(background_pixels - mean_background).^2);
-            t_weight = foreground_pixels .* min(1, 1./(foreground_pixels - mean_target).^2);
+            s_weight = node_intensities .* min(1, 1./(node_intensities - mean_background).^2);
+            t_weight = node_intensities .* min(1, 1./(node_intensities - mean_target).^2);
         
         otherwise
-            s_weight = zeros(num_pixels,1) + 100;
-            t_weight = ones(num_pixels,1);
+            disp('Strict cutoff')
+            s_weight = node_intensities;
+            t_weight = node_intensities;
+            
+            s_weight(s_weight > mean_target) = 0;
+            t_weight(t_weight < mean_target) = 0;
        
     end
     
+    % Adding a super source with Pixel Index = Num Pixels + 1
+    super_src = zeros(num_nodes,1) + num_pixels + 1;
     
-    background_nodes = find(nonzero_pixels == 1);
-    foreground_nodes = background_nodes;
+    % Adding a sink with Pixel Index = Num Pixels + 2
+    sink = zeros(num_nodes,1) + num_pixels + 2;
+
     
-    G = addedge(G, super_src, background_nodes, s_weight);
-    G = addedge(G, foreground_nodes, sink, t_weight);
+    G = addedge(G, super_src, nodes, s_weight);
+    G = addedge(G, nodes, sink, t_weight);
     
     super_src = super_src(1);
     sink = sink(1);
--- skull_stripping/strip.m
+++ skull_stripping/strip.m
@@ -9,7 +9,7 @@ I = imgaussfilt(I,0.5);
 % [Gx,Gy] = imgradientxy(I);
 dim = size(I,1);
 
-% Build Test Image
+% % Build Test Image
 % fname = 'test.png';
 % dim=16; radius=4;
 % insertCircles(dim,1,radius,fname,0,0);
@@ -20,8 +20,8 @@ if ~ismatrix(I)
     I = rgb2gray(I);
 end
 
-% cutoff = 20;
-% I(I < cutoff) = 0;
+cutoff = 15;
+I(I < cutoff) = 0;
 
 figure(1);
 imagesc(I); colormap(gray); set(gca,'YDir','normal');
@@ -30,7 +30,7 @@ imagesc(I); colormap(gray); set(gca,'YDir','normal');
 original_img = I;
 
 % Building graph
-[G, super_src, sink] = constructGraph(dim, original_img, 4)
+[G, super_src, sink] = constructGraph(dim, original_img, 3)
 
 % Run max flow
 [mf,GF,cs,ct] = maxflow(G, super_src, sink);
@@ -49,12 +49,12 @@ src_y = G.Nodes.y(src_nodes);
 
 
 hold on;
-s = scatter(x_vals,y_vals,15,'Filled');
-s.MarkerFaceColor = 'r';
-% s = scatter(src_x,src_y,15,'Filled', 'MarkerFaceColor','b');
-% s.MarkerFaceColor = 'r';
+s = scatter(x_vals,y_vals,5,'Filled','MarkerFaceColor','r');
+% s = scatter(src_x,src_y,10,'Filled', 'MarkerFaceColor','b');
 
 hold off;
+
+
 
 % Change figure size here
 
